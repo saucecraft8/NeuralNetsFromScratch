02d34dbeb33fa81d82c8db9681cb644d56ca307d
Ketan Raghu <ketanaliraghu@gmail.com>
2023-08-08 01:28:06 -0400

    1: Intro and Neuron Code Completed

---
+++ Base Python/IntroAndNeuron.py
@@ -0,0 +1,10 @@
+# Random values
+inputs = [1 , 2 , 3] # Inputs from first layer
+weights = [0.2 , 0.8 , -0.5]
+bias = 2
+
+# Hidden Layer Neuron output
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+print(output)
+
+
