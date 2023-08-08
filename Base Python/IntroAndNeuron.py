575f376f251f54f986af2fc796a6d162b029edb9
Ketan Raghu <ketanaliraghu@gmail.com>
2023-08-08 01:45:42 -0400

    2: Coding a Layer Completed

--- Base Python/IntroAndNeuron.py
+++ Base Python/IntroAndNeuron.py
@@ -1,10 +1,13 @@
 # Random values
-inputs = [1 , 2 , 3] # Inputs from first layer
-weights = [0.2 , 0.8 , -0.5]
+inputs = [1.0 , 2.0 , 3.0 , 2.5] # Inputs from first layer
+weights = [0.2 , 0.8 , -0.5 , 1.0]
 bias = 2
 
 # Hidden Layer Neuron output
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+# Sum(inputs * weights) + bias --> dot(inputs, weights) + bias
+output =   inputs[0]*weights[0]\
+         + inputs[1]*weights[1]\
+         + inputs[2]*weights[2]\
+         + inputs[3]*weights[3]\
+         + bias
 print(output)
-
-
